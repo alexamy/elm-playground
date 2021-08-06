1692c26933e39ce9d1a57d70c83e1a95d7b80389
Alexander Chaschin <achasch@gmail.com>
2021-08-06 09:35:03 +0300

    Refactor: Temperature (#4)
    Replace helper functions with pipes.

--- src/TemperatureConverter.elm
+++ src/TemperatureConverter.elm
@@ -5,11 +5,6 @@ When the user enters a numerical value into TC the corresponding value in TF is 
 When the user enters a non-numerical string into TC the value in TF is not updated and vice versa.
 The formula for converting a temperature C in Celsius into a temperature F in Fahrenheit is C = (F - 32) * (5/9)
 and the dual direction is F = C * (9/5) + 32.
-
----I will skip that part---
-When the user enters a non-numerical string into TC the value in TF is not updated and vice versa.
-
-Why do I need to have non-meaningful values anyway?
 -}
 
 module TemperatureConverter exposing (..)
@@ -17,8 +12,7 @@ module TemperatureConverter exposing (..)
 import Browser
 import Html exposing (Html, div, text, span, input)
 import Html.Attributes exposing (value, style)
-import Html.Events exposing (onInput)
-import Html.Events exposing (onFocus)
+import Html.Events exposing (onInput, onFocus)
 
 celsiusFromFahrenheit : Float -> Float
 celsiusFromFahrenheit f =
@@ -49,25 +43,27 @@ type Msg
   = Celsius String
   | Fahrenheit String
 
-canParse : String -> Bool
-canParse s =
-  case String.toFloat s of
-     Just _ -> True
-     Nothing -> False
-
-tryParseWith : (Float -> Float) -> String -> String -> String
-tryParseWith conversion default value =
-  case String.toFloat value of
-     Just f -> f |> conversion |> String.fromFloat
-     Nothing -> default
-
 update : Msg -> Model -> Model
 update msg model =
   case msg of
     Celsius c ->
-      { model | celsius = c, fahrenheit = tryParseWith fahrenheitFromCelsius model.fahrenheit c }
+      { model
+      | celsius = c
+      , fahrenheit = c
+          |> String.toFloat
+          |> Maybe.map fahrenheitFromCelsius
+          |> Maybe.map String.fromFloat
+          |> Maybe.withDefault model.fahrenheit
+      }
     Fahrenheit f ->
-      { model | fahrenheit = f, celsius = tryParseWith celsiusFromFahrenheit model.celsius f }
+      { model
+      | fahrenheit = f
+      , celsius = f
+          |> String.toFloat
+          |> Maybe.map celsiusFromFahrenheit
+          |> Maybe.map String.fromFloat
+          |> Maybe.withDefault model.celsius
+      }
 
 view : Model -> Html Msg
 view model =
